9ce6b1acab8c98849736cb2385fc56688e0628be
jfoursket <jfoursket@gmail.com>
2024-07-30 09:47:01 +0300

    add preloader

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
-import {ref} from 'vue';
+import {ref, onMounted} from 'vue';
 import Header from "@/components/Header.vue";
 import Hero from "@/components/Hero.vue";
 import Games from "@/components/Games.vue";
 import ContactUs from "@/components/ContactUs.vue";
 import Footer from "@/components/Footer.vue";
 import PopUp from "@/components/PopUp.vue";
+import Preloader from "@/components/Preloader.vue";
 
 const isPopUpOpen = ref(false);
 
@@ -16,9 +17,32 @@ function togglePopUp() {
 function closePopUp() {
   isPopUpOpen.value = false;
 }
+
+const isLoading = ref(true);
+function hidePreloader() {
+  isLoading.value = false;
+}
+
+// Рабочий
+// onMounted(() => {
+//   if (document.readyState === 'complete') {
+//     hidePreloader();
+//   } else {
+//     window.addEventListener('DOMContentLoaded', hidePreloader);
+//     window.addEventListener('load', hidePreloader);
+//   }
+// });
+
+// Для демонстрации
+onMounted(() => {
+  setTimeout(() => {
+    isLoading.value = false;
+  }, 1000);
+});
 </script>
 
 <template>
+  <Preloader v-if="isLoading"/>
   <Header :togglePopUp="togglePopUp"/>
 
 
---
+++ src/components/Preloader.vue
@@ -0,0 +1,9 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="preloader">
+    <div class="spinner"></div>
+  </div>
+</template>
